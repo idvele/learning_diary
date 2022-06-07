6371646822011056aa75e4deed8abb1ebf75913f
idvele <ilari.katajamaki1@gmail.com>
2022-06-07 13:28:54 +0300

    initial commit

---
+++ Learning Diary IK/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Learning_Diary_IK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TODO: Objekteista tehdään List jossa jokaisella Topic-objektilla on indeksin mukainen paikka
+            // Kysymykset on nyt metodina, sen tulokset voisi tallentaa ArrayListiin josta saa tehtyä metodin, jolla tulokset on helppoa printata
+
+            List<Topic> topics = new List<Topic>();
+
+            topics.Add(kysymykset());
+
+           
+
+            foreach (var item in topics)
+            {
+                //tähän looppi arrayListille
+                foreach (var item in mbox)
+                {
+
+                }
+                
+            }
+            Console.ReadKey();
+
+       
+        }
+
+        
+
+        public static Topic kysymykset()
+        {
+            //nÄmä arraylistiin
+            Topic mytopic = new Topic();
+
+            Console.Write("Enter topic Id: ");
+            mytopic.Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Title: ");
+            mytopic.Title = Console.ReadLine();
+
+            Console.WriteLine("Enter Description: ");
+            mytopic.Description = Console.ReadLine();
+
+            Console.WriteLine("Enter ETA to master: ");
+            mytopic.EstimatedTimeToMaster = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Time spent: ");
+            mytopic.TimeSpent = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Source: ");
+            mytopic.Source = Console.ReadLine();
+
+
+            Console.WriteLine("Enter start learning date as dd/mm/yyyy: ");
+            var str = Console.ReadLine();
+            DateTime dt;
+
+            var isValidDate = DateTime.TryParse(str, out dt);
+
+            if (isValidDate)
+            {
+                Console.WriteLine(mytopic.StartLearningDate);
+                mytopic.StartLearningDate = dt;
+            }
+
+            else
+                Console.WriteLine($"{str} is not a valid date string");
+
+            Console.WriteLine("Enter in progress?(1 = yes 2= no : ");
+
+            string answer = Console.ReadLine();
+            if (answer == "1")
+                mytopic.inProgress = true;
+            else if (answer == "2")
+                mytopic.inProgress = false;
+            else
+                mytopic.inProgress = false;
+
+            Console.WriteLine("Enter completion date as dd/mm/yyyy: ");
+            var str1 = Console.ReadLine();
+            DateTime dt1;
+
+            var isValidDate1 = DateTime.TryParse(str, out dt);
+
+            if (isValidDate)
+            {
+                Console.WriteLine(mytopic.CompletionDate);
+                mytopic.CompletionDate = dt;
+            }
+
+            else
+                Console.WriteLine($"{str} is not a valid date string");
+
+            return mytopic;
+
+        }
+
+        
+    }
+}
---
+++ Learning Diary IK/Topic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Learning_Diary_IK
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double EstimatedTimeToMaster { get; set; }
+        public double TimeSpent { get; set; }
+        public string Source { get; set; }
+        public DateTime StartLearningDate { get; set; }
+        public bool inProgress { get; set; }
+        public DateTime CompletionDate { get; set; }
+
+
+        //override mahdollistaa komennon tekemisen classin nimellä
+       
+
+    }
+}
